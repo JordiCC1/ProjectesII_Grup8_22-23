5e1ac1cabd85e3b238920b302c06f1566f1b31e5
Jordi Climent <jordi.climent@enti.cat>
2022-11-03 17:35:26 +0100

    Fixed Rotation/ Added Particles
    The particles doesn't work

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -5,9 +5,16 @@ using UnityEngine;
 public class Bullet : MonoBehaviour
 {
     public float lifeTime;
+    public ParticleSystem ps;
     void Start()
     {
+        //ps = GetComponent<ParticleSystem>();
         StartCoroutine(WaitThenDie());
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        ps.Play();
+        Destroy(gameObject);
     }
     IEnumerator WaitThenDie()
     {
