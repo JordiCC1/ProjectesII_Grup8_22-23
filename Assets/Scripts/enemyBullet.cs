e972ebbae96de6e45c729ed326fef48828c6dfb0
Jordi Climent <jordi.climent@enti.cat>
2022-11-04 16:15:50 +0100

    Updated Enemy

---
+++ Assets/Scripts/enemyBullet.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enemyBullet : MonoBehaviour
+{
+    public float lifeTime;
+    public ParticleSystem ps;
+    void Start()
+    {
+        ps = GetComponent<ParticleSystem>();
+        StartCoroutine(WaitThenDie());
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        ps.Play();
+        Destroy(gameObject);
+    }
+    IEnumerator WaitThenDie()
+    {
+        yield return new WaitForSeconds(lifeTime);
+        Destroy(gameObject);
+    }
+}
