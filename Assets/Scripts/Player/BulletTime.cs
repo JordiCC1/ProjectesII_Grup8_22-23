0806d369b40cf64ee17ed3ccbc780f9ab8233c3d
Pau Amigó <pau.amigo@enti.cat>
2022-12-08 21:57:44 +0100

    Finished implementing Buttons
    Only assets left

--- Assets/Scripts/Player/BulletTime.cs
+++ Assets/Scripts/Player/BulletTime.cs
@@ -4,108 +4,112 @@ using System.Collections;
 namespace Player
 {
 
-    public struct BulletTimeInputs
-    {
-        public bool BulletTimeDown;
-        public bool BulletTimeUp;
-        public bool lastState;
-    }
+	public struct BulletTimeInputs
+	{
+		public bool BulletTimeDown;
+		public bool BulletTimeUp;
+		public bool lastState;
+	}
 
-    public class BulletTime : MonoBehaviour
-    {
-        [Header("Bullet Time")]
-        public float slowdownFactor = 0.2f;
-        private float normalTimeScale = 1.0f;
-        private float actualTimeScale;
+	public class BulletTime : MonoBehaviour
+	{
+		[Header("Bullet Time")]
+		public float slowdownFactor = 0.2f;
+		private float normalTimeScale = 1.0f;
+		private float actualTimeScale;
 
-        public bool isActive = false;
+		public bool isActive = false;
 
-        public static BulletTime instance;
+		public static BulletTime instance;
 
-        public float timeToNormal=.75f;
+		public float timeToNormal = .75f;
+
+        [SerializeField] PauseMenu pauseMenu;
 
         [Header("Interpolation")]
-        Interpolator lerp;
-        public AnimationCurve curve;
+		Interpolator lerp;
+		public AnimationCurve curve;
 
-        private void Awake()
-        {
-            if (instance == null)
-            {
-                instance = this;
-            }
-            else
-            {
-                Destroy(gameObject);
-            }
+		private void Awake()
+		{
+			if (instance == null)
+			{
+				instance = this;
+			}
+			else
+			{
+				Destroy(gameObject);
+			}
 
-            lerp = new Interpolator(timeToNormal);
-        }
+			lerp = new Interpolator(timeToNormal);
+		}
 
-        public void UpdateBulletTime(BulletTimeInputs inputs, bool canBT)
-        {
-            if (inputs.BulletTimeUp)
-            {
-                this.gameObject.GetComponentInChildren<Gun>().Shoot();
-            }
-            if (canBT)
-            { 
-                if (StaminaController.instance.stamina >= 0.0f)
-                {
-                    if (inputs.BulletTimeDown)
-                    {
-                        BulletTimeActive();
-                        StaminaController.instance.UseStamina();
-                    }
-                    else if (inputs.BulletTimeUp)
-                    {
-                        FinishBulletTime();
-                        StaminaController.instance.StopStamina();
-                    }
-                }
-                else
-                {
-                    FinishBulletTime();
-                    StaminaController.instance.StopStamina();
-                }
-            }
-            else
-            {
+		public void UpdateBulletTime(BulletTimeInputs inputs, bool canBT)
+		{
+			if (inputs.BulletTimeUp)
+			{
+				this.gameObject.GetComponentInChildren<Gun>().Shoot();
+			}
+			if (canBT && !pauseMenu.isPaused)
+			{
+				if (StaminaController.instance.stamina >= 0.0f)
+				{
+					if (inputs.BulletTimeDown)
+					{
+						BulletTimeActive();
+						StaminaController.instance.UseStamina();
+					}
+					else if (inputs.BulletTimeUp)
+					{
+						FinishBulletTime();
+						StaminaController.instance.StopStamina();
+					}
+					Time.timeScale = actualTimeScale;
+				}
+				else
+				{
+					FinishBulletTime();
+					StaminaController.instance.StopStamina();
+					Time.timeScale = actualTimeScale;
+				}
+			}
+			else if (!pauseMenu.isPaused)
+			{
                 FinishBulletTime();
                 StaminaController.instance.StopStamina();
             }
-            Time.timeScale = actualTimeScale;
-        }
 
-        void BulletTimeActive()
-        {
-            Time.timeScale = slowdownFactor;
-            actualTimeScale = slowdownFactor;
-            isActive = true;
-            AudioManager.instance.ChangePitch(0.8f);
-        }
+		}
 
-        void FinishBulletTime()
-        {
-            actualTimeScale = 1.0f;
-            Time.timeScale =actualTimeScale;
-            isActive = false;
-            StaminaController.instance.ResetStamina();
-            AudioManager.instance.ChangePitch(1.0f);
-        }
+		void BulletTimeActive()
+		{
+			Time.timeScale = slowdownFactor;
+			actualTimeScale = slowdownFactor;
+			isActive = true;
+			AudioManager.instance.ChangePitch(0.8f);
+		}
 
-        public void BackToNormal()
-        {
-            lerp.Update(Time.deltaTime);
+		void FinishBulletTime()
+		{
+			actualTimeScale = 1.0f;
+			Time.timeScale = actualTimeScale;
+			isActive = false;
+			StaminaController.instance.ResetStamina();
+			AudioManager.instance.ChangePitch(1.0f);
+		}
 
-            if (lerp.IsMaxPrecise)
-                lerp.ToMin();
+		public void BackToNormal()
+		{
+			lerp.Update(Time.deltaTime);
 
-            else if (lerp.IsMinPrecise)
-                lerp.ToMax();
+			if (lerp.IsMaxPrecise)
+				lerp.ToMin();
 
-            actualTimeScale = Mathf.Lerp(actualTimeScale, normalTimeScale, curve.Evaluate(lerp.Value));
-        }
+			else if (lerp.IsMinPrecise)
+				lerp.ToMax();
 
-    }
+			actualTimeScale = Mathf.Lerp(actualTimeScale, normalTimeScale, curve.Evaluate(lerp.Value));
+		}
+
+	}
 }
