17f2c689819147b923e6982272e3356e3c69a89a
Jordi Climent <jordi.climent@enti.cat>
2023-02-24 20:21:08 +0100

    Checkpoint fixed

--- Assets/Scripts/CheckpointMaster.cs
+++ Assets/Scripts/CheckpointMaster.cs
@@ -1,12 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class CheckpointMaster : MonoBehaviour
 {
     private static CheckpointMaster instance;
 
     public Vector2 lastCheckPointPos;
+
+    private Vector2[] firstCheckPoints;
     private void Awake()
     {
         if(instance == null) 
@@ -19,4 +22,5 @@ public class CheckpointMaster : MonoBehaviour
             Destroy(gameObject);
         }
     }
+    
 }
--- Assets/Scripts/ControlScene.cs
+++ Assets/Scripts/ControlScene.cs
@@ -8,9 +8,11 @@ public class ControlScene : MonoBehaviour
     public int index;
     public string levelName;
 
+    private CheckpointMaster cm;
+
     private void OnTriggerEnter2D(Collider2D other)
     {
-
+        cm = GameObject.FindGameObjectWithTag("CM").GetComponent<CheckpointMaster>();
         if (other.CompareTag("Player"))
         {
             StartCoroutine("Wait");
@@ -19,6 +21,8 @@ public class ControlScene : MonoBehaviour
 
     IEnumerator Wait()
     {
+        Destroy(cm);
+
         yield return new WaitForSeconds(0.7f);
 
         SceneManager.LoadScene(index);
