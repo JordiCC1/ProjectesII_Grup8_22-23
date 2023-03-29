f8861732e16c7c0c51cda74ca4b245f56b1394ca
Aniol <aniol.morato@enti.cat>
2023-03-29 17:50:15 +0200

    Finished Shield

--- Assets/Scripts/Enemies/enemyBullet.cs
+++ Assets/Scripts/Enemies/enemyBullet.cs
@@ -19,6 +19,19 @@ public class enemyBullet : MonoBehaviour
         ParticleIns.GetComponent<ParticleSystem>().Play();
         Destroy(gameObject);
     }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Shield"))
+        {
+            rb.AddForce(-force * 2);
+            SetForce(-force);
+            //Destroy(gameObject);
+
+            //objCollided = Instantiate(bullet, aux.transform.position, Quaternion.identity);
+            //objCollided.GetComponent<Rigidbody2D>().AddForce(-aux.GetComponent<enemyBullet>().GetForce());
+            //objCollided.GetComponent<enemyBullet>().SetForce(-aux.GetComponent<enemyBullet>().GetForce());
+        }
+    }
     IEnumerator WaitThenDie()
     {
         yield return new WaitForSeconds(lifeTime);
--- Assets/Scripts/Player/Shield.cs
+++ Assets/Scripts/Player/Shield.cs
@@ -13,6 +13,7 @@ namespace Player
         public bool isShieldUp;
         public GameObject shield;
         [SerializeField] StaminaController staminaController;
+        [SerializeField] private GameObject bullet;
 
         // Start is called before the first frame update
         void Start()
@@ -40,12 +41,14 @@ namespace Player
         private void OnCollisionEnter2D(Collision2D collision)
         {
             GameObject objCollided = collision.gameObject;
+            //GameObject aux = objCollided;
+            Debug.Log("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAa");
             if (objCollided.CompareTag("Bullet"))
             {
-                Vector2 aux = objCollided.GetComponent<enemyBullet>().GetForce();
-                aux.x -= 2 * aux.x;
-                aux.y -= 2 * aux.y;
-                objCollided.GetComponent<Rigidbody2D>().AddForce(aux);
+                Destroy(collision.gameObject);
+                //objCollided = Instantiate(bullet, aux.transform.position, Quaternion.identity);
+                //objCollided.GetComponent<Rigidbody2D>().AddForce(-aux.GetComponent<enemyBullet>().GetForce());
+                //objCollided.GetComponent<enemyBullet>().SetForce(-aux.GetComponent<enemyBullet>().GetForce());
             }
         }
     }
